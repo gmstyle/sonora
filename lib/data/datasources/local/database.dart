2b371a0ced9125f51d75cb5d5c2b50367e8dc259
Gabriele Martina <contattagmstyle@gmail.com>
2026-06-02 20:23:55 +0200

    Fix db path in Linux

--- lib/data/datasources/local/database.dart
+++ lib/data/datasources/local/database.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:drift/drift.dart';
 import 'package:drift_flutter/drift_flutter.dart';
 import 'package:path_provider/path_provider.dart';
@@ -89,8 +91,11 @@ class AppDatabase extends _$AppDatabase {
 QueryExecutor _openConnection() {
   return driftDatabase(
     name: 'sonora',
-    native: const DriftNativeOptions(
-      databaseDirectory: getApplicationDocumentsDirectory,
+    native: DriftNativeOptions(
+      databaseDirectory:
+          Platform.isLinux
+              ? getApplicationSupportDirectory
+              : getApplicationDocumentsDirectory,
     ),
   );
 }
